47bac546c9e13456138e5eca74c54e40656a65f0
neves350 <martimneves350@gmail.com>
2025-12-03 18:12:04 +0000

    class 1

---
+++ server.ts
@@ -0,0 +1,66 @@
+import fastify from 'fastify'
+import crypto from 'node:crypto'
+
+const server = fastify({
+	logger: {
+		// Dá um log no terminal
+		transport: {
+			target: 'pino-pretty',
+			options: {
+				translateTime: 'HH:MM:ss Z',
+				ignore: 'pid,hostname',
+			},
+		},
+	},
+})
+
+const courses = [
+	{ id: '1', title: 'Curso de Node.js' },
+	{ id: '2', title: 'Curso de React' },
+	{ id: '3', title: 'Curso de Angular' },
+]
+
+// Routes
+server.get('/courses', () => {
+	return { courses }
+})
+
+server.get('/courses/:id', (request, reply) => {
+	type Params = {
+		id: string
+	}
+
+	const params = request.params as Params
+	const courseId = params.id
+
+	const course = courses.find((course) => course.id === courseId)
+
+	if (course) {
+		return { course }
+	}
+
+	return reply.status(404)
+})
+
+server.post('/courses', (request, reply) => {
+	type Body = {
+		title: string
+	}
+
+	const courseId = crypto.randomUUID()
+	const body = request.body as Body
+	const courseTitle = body.title
+
+	if (!courseTitle) {
+		return reply.status(400).send({ message: 'Título obrigatório!' })
+	}
+
+	courses.push({ id: courseId, title: courseTitle })
+
+	// SEMPRE retornar um objeto nas rotas
+	return reply.status(201).send({ courseId })
+})
+
+server.listen({ port: 3333 }).then(() => {
+	console.log('HTTP Server running')
+})
